e1412055bf2077c4f258e459127e9762c18b9689
John Tomaselli <jrt@gtz.com>
2018-09-09 10:02:09 -0400

    105

--- src/registration-form.js
+++ src/registration-form.js
@@ -2,7 +2,8 @@ import {inject} from 'aurelia-dependency-injection';
 import {
   ValidationControllerFactory,
   ValidationController,
-  ValidationRules
+  ValidationRules,
+  validateTrigger
 } from 'aurelia-validation';
 import {BootstrapFormRenderer} from './bootstrap-form-renderer';
 
@@ -11,20 +12,31 @@ export class RegistrationForm {
   firstName = '';
   lastName = '';
   email = '';
+ 
   controller = null;
-  
+  user = new User();
+
   constructor(controllerFactory) {
     this.controller = controllerFactory.createForCurrentScope();
     this.controller.addRenderer(new BootstrapFormRenderer());
+      this.controller.addObject(this);
+    this.controller.addObject(this.user);
   }
   
   submit() {
     this.controller.validate();
   }
 }
-
+export class User {
+  fname;
+  lname;
+}
 ValidationRules
   .ensure(a => a.firstName).required()
   .ensure(a => a.lastName).required()
   .ensure(a => a.email).required().email()
   .on(RegistrationForm);
+
+  // ValidationRules
+  // .ensure(a => a.address).required()
+  // .on(App);
